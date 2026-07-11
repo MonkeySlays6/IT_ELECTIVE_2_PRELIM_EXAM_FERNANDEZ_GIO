8cdb371a0d4bff2a9b907c45379bc5e719d51902
ehhh <monkeydummyacc@gmail.com>
2026-07-11 09:34:40 +0800

    Update GetMealById.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 3: GET Lookup by ID
@@ -14,11 +17,21 @@ public static class GetMealById
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52771
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the meal name (strMeal) is "Arrabiata"
+        string url = "https://themealdb.com/api/json/v1/1/lookup.php?i=52771";
+        
+        HttpResponseMessage response = await client.GetAsync(url);
 
-        throw new NotImplementedException();
+        if(response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception($"Expected status code 200 OK, but got {response.StatusCode}");
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        JsonDocument document = JsonDocument.Parse(responseBody);
+
+        JsonElement meal = document.RootElement.GetProperty("meals")[0];
+
+        string mealName = meal.GetProperty("strMeal").GetString();
     }
 }
