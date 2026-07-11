2161a8c08c0f7c2d948d92ca13ded56a6101c2a0
ehhh <monkeydummyacc@gmail.com>
2026-07-11 09:41:11 +0800

    Update CreateReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 6: POST Create Review
@@ -15,15 +19,33 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class CreateReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Create JSON string with title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send POST request to https://jsonplaceholder.typicode.com/posts
-        // TODO: Assert status code is 201 Created
-        // TODO: Parse the response JSON
-        // TODO: Assert the response has an "id" field with a value
+        string url = "https://jsonplaceholder.typicode.com/posts";
 
-        throw new NotImplementedException();
+        string json = @"{
+            ""title"": ""Great Pasta!"",
+            ""body"": ""Loved this recipe"",
+            ""userId"": 1
+        }";
+
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync(url, content);
+
+        if (response.StatusCode != HttpStatusCode.Created)
+        {
+            throw new Exception($"Expected status code 201 Created, but got {response.StatusCode}");
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument document = JsonDocument.Parse(responseBody);
+
+        if (!document.RootElement.TryGetProperty("id", out JsonElement id) ||
+            id.ValueKind == JsonValueKind.Null)
+        {
+            throw new Exception("Response does not contain a valid 'id' field.");
+        }
     }
 }
