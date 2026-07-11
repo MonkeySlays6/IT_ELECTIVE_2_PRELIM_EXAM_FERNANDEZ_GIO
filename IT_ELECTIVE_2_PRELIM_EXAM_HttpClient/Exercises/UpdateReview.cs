e94fc5c6ddb1c786849b01a8bd8145a7470b040e
ehhh <monkeydummyacc@gmail.com>
2026-07-11 09:43:21 +0800

    Update UpdateReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 7: PUT Update Review
@@ -14,15 +18,35 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class UpdateReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Create JSON string with id, title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send PUT request to https://jsonplaceholder.typicode.com/posts/1
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the title is "Updated Review"
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
 
-        throw new NotImplementedException();
+        string json = @"{
+            ""id"": 1,
+            ""title"": ""Updated Review"",
+            ""body"": ""Even better than before!"",
+            ""userId"": 1
+        }";
+
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PutAsync(url, content);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception($"Expected status code 200 OK, but got {response.StatusCode}");
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument document = JsonDocument.Parse(responseBody);
+
+        string? title = document.RootElement.GetProperty("title").GetString();
+
+        if (title != "Updated Review")
+        {
+            throw new Exception($"Expected title 'Updated Review', but got '{title}'.");
+        }
     }
 }
