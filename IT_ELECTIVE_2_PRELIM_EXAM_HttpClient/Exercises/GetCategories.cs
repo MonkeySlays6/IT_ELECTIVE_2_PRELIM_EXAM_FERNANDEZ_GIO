ec48b01cfd1602eb5812a1c910e3134140d79b89
ehhh <monkeydummyacc@gmail.com>
2026-07-11 09:36:55 +0800

    Update GetCategories.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Text.Json;
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 4: GET List Categories
@@ -9,16 +11,31 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 // 3. Parse the JSON and assert the "categories" array has more than 0 items
 //
 // Response format: { "categories": [{ "strCategory": "Beef", ... }, ...] }
-
 public static class GetCategories
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/categories.php
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the "categories" array has more than 0 items
+        string url = "https://themealdb.com/api/json/v1/1/categories.php";
 
-        throw new NotImplementedException();
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception($"Expected status code 200 OK, but got {response.StatusCode}");
+        }
+
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+
+        JsonDocument document = JsonDocument.Parse(responseBody);
+
+
+        JsonElement categories = document.RootElement.GetProperty("categories");
+
+        if (categories.GetArrayLength() <= 0)
+        {
+            throw new Exception("Expected one or more categories, but none were found.");
+        }
     }
 }
